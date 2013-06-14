8d81820032c41fedddfbfa4f718a0585887bebf2
Jalir Duran <jayniw@gmail.com>
2013-06-14 12:12:39 -0400

    creacion MercadianMainBundle
    se crea el bundle para la creacion del projecto, se instacion al bundle
    theme para que jale su configuracion de css y js

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             new Sonata\MediaBundle\SonataMediaBundle(),
             new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+            new Mercadian\MainBundle\MercadianMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
